eac4dd42358ef2dcf774f3a1eb98089f8ec9a4ff
ChachyDev <shalomadecoolboy@outlook.com>
2020-11-28 18:04:14 +0000

    Kill off coroutines

--- src/main/kotlin/club/chachy/simpleconfig/Config.kt
+++ src/main/kotlin/club/chachy/simpleconfig/Config.kt
@@ -4,10 +4,6 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.io.File
 import java.lang.reflect.Field
 
@@ -24,53 +20,45 @@ class Config(private val file: File, private val `class`: Any, isPrettyPrinted: 
 
     private val configClass = `class`::class.java
 
-    private val configScope = CoroutineScope(CoroutineName("Config Coroutine") + Dispatchers.IO)
-
-    suspend fun load() {
-        configScope.launch {
-            if (!file.exists()) error("The file given does not exist.")
-            val text = file.readText()
-            try {
-                if (text.isNotEmpty()) {
-                    obj = JsonParser.parseString(text).asJsonObject
-                    for (i in configClass.declaredFields.size - 1 downTo 0) {
-                        val field = configClass.declaredFields[i]
-                        if (field.isAnnotationPresent(ConfigOption::class.java)) {
-                            if (obj.has(field.name)) {
-                                field.isAccessible = true
-                                field.set(`class`, gson.fromJson(obj.get(field.name), field.type))
-                            }
+    fun load() {
+        if (!file.exists()) error("The file given does not exist.")
+        val text = file.readText()
+        try {
+            if (text.isNotEmpty()) {
+                obj = JsonParser.parseString(text).asJsonObject
+                for (i in configClass.declaredFields.size - 1 downTo 0) {
+                    val field = configClass.declaredFields[i]
+                    if (field.isAnnotationPresent(ConfigOption::class.java)) {
+                        if (obj.has(field.name)) {
+                            field.isAccessible = true
+                            field.set(`class`, gson.fromJson(obj.get(field.name), field.type))
                         }
                     }
                 }
-            } catch (e: Exception) {
-                println("An error occurred trying to load the config")
             }
-        }.join()
+        } catch (e: Exception) {
+            println("An error occurred trying to load the config")
+        }
     }
 
-    suspend fun save() {
-        configScope.launch {
-            for (i in configClass.declaredFields.size - 1 downTo 0) {
-                val field = configClass.declaredFields[i]
-                if (field.isAnnotationPresent(ConfigOption::class.java)) {
-                    save(field)
-                }
+    fun save() {
+        for (i in configClass.declaredFields.size - 1 downTo 0) {
+            val field = configClass.declaredFields[i]
+            if (field.isAnnotationPresent(ConfigOption::class.java)) {
+                save(field)
             }
-            file.writeText(gson.toJson(obj))
-        }.join()
+        }
+        file.writeText(gson.toJson(obj))
     }
 
 
-    private suspend fun save(field: Field) {
-        configScope.launch {
-            try {
-                field.isAccessible = true
-                obj.add(field.name, gson.toJsonTree(field.get(`class`), field.type))
-            } catch (e: Exception) {
-                println("An error occurred when trying to save ${field.name}")
-            }
-        }.join()
+    private fun save(field: Field) {
+        try {
+            field.isAccessible = true
+            obj.add(field.name, gson.toJsonTree(field.get(`class`), field.type))
+        } catch (e: Exception) {
+            println("An error occurred when trying to save ${field.name}")
+        }
     }
 }
 
